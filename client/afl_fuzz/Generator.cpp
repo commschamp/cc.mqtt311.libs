5d3f78496f938d3b0a50db274eddcb3d5a8f45e2
Alex Robenko <arobenko@gmail.com>
2024-04-24 08:38:20 +1000

    Added fuzzing application.

---
+++ client/afl_fuzz/Generator.cpp
@@ -0,0 +1,215 @@
+//
+// Copyright 2024 - 2024 (C). Alex Robenko. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "Generator.h"
+
+#include "comms/process.h"
+
+#include <cassert>
+#include <type_traits>
+
+namespace cc_mqtt311_client_afl_fuzz
+{
+
+namespace 
+{
+
+std::string pubTopicFromFilter(const std::string& filter)
+{
+    std::string result;
+    std::size_t pos = 0U;
+    while (pos < filter.size()) {
+        auto wildcardPos = filter.find_first_of("#+", pos);
+        if (wildcardPos == std::string::npos) {
+            break;
+        }
+
+        result.append(filter.substr(pos, wildcardPos - pos));
+        pos = wildcardPos + 1U;
+        
+        if (filter[wildcardPos] == '#') {
+            result.append("hash");
+            pos = filter.size();
+            break;
+        }
+
+        assert(filter[wildcardPos] == '+');
+        result.append("plus");
+    }
+
+    if (pos < filter.size()) {
+        result.append(filter.substr(pos));
+    }
+
+    return result;
+}
+
+} // namespace 
+    
+
+bool Generator::prepare(const std::string& inputFile)
+{
+    m_stream.open(inputFile);
+    if (!m_stream) {
+        m_logger.errorLog() << "Failed to open " << inputFile << " for writing" << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+void Generator::processData(const std::uint8_t* buf, unsigned bufLen)
+{
+    [[maybe_unused]] auto consumed = comms::processAllWithDispatch(buf, bufLen, m_frame, *this);
+    assert(consumed == bufLen);
+}
+
+void Generator::handle(const Mqtt311ConnectMsg& msg)
+{
+    m_logger.infoLog() << "Processing " << msg.name() << "\n";
+    Mqtt311ConnackMsg outMsg;
+    sendMessage(outMsg);
+    m_connected = true;
+}
+
+void Generator::handle(const Mqtt311PublishMsg& msg)
+{
+    m_logger.infoLog() << "Processing " << msg.name() << "\n";
+
+    using QosValueType = Mqtt311PublishMsg::TransportField_flags::Field_qos::ValueType;
+    auto qos = msg.transportField_flags().field_qos().value();
+    if (qos == QosValueType::AtMostOnceDelivery) {
+        m_logger.infoLog() << "at most once" << "\n";
+        doNextPublishIfNeeded();
+        return;
+    }
+
+    if (qos == QosValueType::AtLeastOnceDelivery) {
+        Mqtt311PubackMsg outMsg;
+        outMsg.field_packetId().setValue(msg.field_packetId().field().getValue());
+        sendMessage(outMsg);
+        doNextPublishIfNeeded();
+        return;
+    }
+
+    assert(qos == QosValueType::ExactlyOnceDelivery);
+    Mqtt311PubrecMsg outMsg;
+    outMsg.field_packetId().setValue(msg.field_packetId().field().getValue());
+    sendMessage(outMsg);
+    return;
+}
+
+void Generator::handle(const Mqtt311PubrecMsg& msg)
+{
+    m_logger.infoLog() << "Processing " << msg.name() << "\n";
+    Mqtt311PubrelMsg outMsg;
+    outMsg.field_packetId().setValue(msg.field_packetId().getValue());
+    sendMessage(outMsg);
+    return;    
+}
+
+void Generator::handle(const Mqtt311PubrelMsg& msg)
+{
+    m_logger.infoLog() << "Processing " << msg.name() << "\n";
+    Mqtt311PubcompMsg outMsg;
+    outMsg.field_packetId().setValue(msg.field_packetId().getValue());
+    sendMessage(outMsg);
+    doNextPublishIfNeeded();
+    return;    
+}
+
+void Generator::handle(const Mqtt311SubscribeMsg& msg)
+{
+    m_logger.infoLog() << "Processing " << msg.name() << "\n";
+    auto& subsVec = msg.field_list().value();
+
+    Mqtt311SubackMsg outMsg;
+    outMsg.field_packetId().value() = msg.field_packetId().value();
+    auto& ackVec = outMsg.field_list().value();
+    ackVec.resize(subsVec.size());
+
+    for (auto& elem : ackVec) {
+        using ElemType = std::decay_t<decltype(elem)>;
+        elem.setValue(ElemType::ValueType::Qos2);
+    }
+
+    sendMessage(outMsg);
+
+    for (auto& subElemField : subsVec) {
+        m_lastPubTopic = pubTopicFromFilter(subElemField.field_topic().value());
+        doPublish();
+    }
+}
+
+void Generator::handle(const Mqtt311UnsubscribeMsg& msg)
+{
+    m_logger.infoLog() << "Processing " << msg.name() << "\n";
+    Mqtt311UnsubackMsg outMsg;
+    outMsg.field_packetId().value() = msg.field_packetId().value();
+    sendMessage(outMsg);
+}
+
+void Generator::handle([[maybe_unused]] const Mqtt311Message& msg)
+{
+    m_logger.infoLog() << "Ignoring " << msg.name() << "\n";
+}
+
+unsigned Generator::allocPacketId()
+{
+    ++m_lastPacketId;
+    return m_lastPacketId;
+}
+
+void Generator::sendMessage(Mqtt311Message& msg)
+{
+    m_logger.infoLog() << "Generating " << msg.name() << "\n";
+    msg.refresh();
+    RawDataBuf outBuf;
+    outBuf.reserve(m_frame.length(msg));
+    auto iter = std::back_inserter(outBuf);
+    [[maybe_unused]] auto es = m_frame.write(msg, iter, outBuf.max_size());
+    assert(es == comms::ErrorStatus::Success);
+    assert(m_dataReportCb);
+    
+    std::ostreambuf_iterator<char> outIter(m_stream);
+    std::copy(outBuf.begin(), outBuf.end(), outIter);
+    m_dataReportCb(outBuf.data(), outBuf.size());
+}
+
+void Generator::sendPublish(const std::string& topic, unsigned qos)
+{
+    Mqtt311PublishMsg outMsg;
+
+    outMsg.transportField_flags().field_qos().setValue(qos);
+    outMsg.field_topic().setValue(topic);
+    if (qos > 0U) {
+        outMsg.field_packetId().field().setValue(allocPacketId());
+    }
+    outMsg.field_payload().value().assign(topic.begin(), topic.end());
+
+    sendMessage(outMsg);
+    ++m_pubCount;
+}
+
+void Generator::doPublish()
+{
+    assert(!m_lastPubTopic.empty());
+    sendPublish(m_lastPubTopic, m_nextPubQos);
+    ++m_nextPubQos;
+    if (m_nextPubQos > 2) {
+        m_nextPubQos = 0;
+    }
+} 
+
+void Generator::doNextPublishIfNeeded()
+{
+    if (m_pubCount < m_minPubCount) {
+        doPublish();
+    }
+}
+
+} // namespace cc_mqtt311_client_afl_fuzz
---
+++ client/afl_fuzz/Generator.h
@@ -0,0 +1,92 @@
+//
+// Copyright 2024 - 2024 (C). Alex Robenko. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include "Logger.h"
+
+#include "cc_mqtt311/Message.h"
+#include "cc_mqtt311/frame/Frame.h"
+
+#include "comms/options.h"
+
+#include <cstdint>
+#include <iterator>
+#include <fstream>
+#include <functional>
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace cc_mqtt311_client_afl_fuzz
+{
+
+class Generator
+{
+public:
+    using RawDataBuf = std::vector<std::uint8_t>;
+
+    using Mqtt311Message = 
+        cc_mqtt311::Message<
+            comms::option::app::ReadIterator<const std::uint8_t*>,
+            comms::option::app::WriteIterator<std::back_insert_iterator<RawDataBuf>>,
+            comms::option::app::LengthInfoInterface,
+            comms::option::app::IdInfoInterface,
+            comms::option::app::NameInterface,
+            comms::option::app::RefreshInterface,
+            comms::option::app::Handler<Generator>
+        >;
+
+    CC_MQTT311_ALIASES_FOR_ALL_MESSAGES_DEFAULT_OPTIONS(Mqtt311, Msg, Mqtt311Message);
+
+    using DataReportCb = std::function<void (const std::uint8_t* buf, std::size_t bufLen)>;
+
+    Generator(Logger& logger, unsigned minPubCount) : m_logger(logger), m_minPubCount(minPubCount) {};
+
+    bool prepare(const std::string& inputFile);
+    void processData(const std::uint8_t* buf, unsigned bufLen);
+
+    template <typename TFunc>
+    void setDataReportCb(TFunc&& func)
+    {
+        m_dataReportCb = std::forward<TFunc>(func);
+    }
+
+    void handle(const Mqtt311ConnectMsg& msg);
+    void handle(const Mqtt311PublishMsg& msg);
+    void handle(const Mqtt311PubrecMsg& msg);
+    void handle(const Mqtt311PubrelMsg& msg);
+    void handle(const Mqtt311SubscribeMsg& msg);
+    void handle(const Mqtt311UnsubscribeMsg& msg);
+    void handle(const Mqtt311Message& msg);
+
+private:
+    using Mqtt311Frame = cc_mqtt311::frame::Frame<Mqtt311Message>;
+
+    unsigned allocPacketId();
+    void sendMessage(Mqtt311Message& msg);
+    void sendPublish(const std::string& topic, unsigned qos);
+    void doPublish();
+    void doNextPublishIfNeeded();
+
+    Logger& m_logger;
+    unsigned m_minPubCount = 0U;
+    std::ofstream m_stream;
+    DataReportCb m_dataReportCb;
+    Mqtt311Frame m_frame;
+    std::unique_ptr<Mqtt311ConnectMsg> m_cachedConnect;
+    unsigned m_lastPacketId = 0U;
+    unsigned m_nextPubQos = 0U;
+    std::string m_lastPubTopic;
+    unsigned m_pubCount = 0U;
+    bool m_connected = false;
+};
+
+using GeneratorPtr = std::unique_ptr<Generator>;
+
+} // namespace cc_mqtt311_client_afl_fuzz
